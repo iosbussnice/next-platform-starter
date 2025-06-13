9117e8ab3e27d19f8294bc04afdac25dc6dbb1b2
iosbussnice <mobilebusiness363@gmail.com>
2025-06-13 22:38:20 +0100

    Update page.jsx

--- app/classics/page.jsx
+++ app/classics/page.jsx
@@ -1,1 +1,14 @@
-hello
+import EdgeFunctionExplainer from '../explainer';
+
+export const metadata = {
+    title: 'app-ads.txt'
+};
+
+export default function Page() {
+    return (
+        <>
+            <h1 className="mb-8">hello!</h1>
+            <EdgeFunctionExplainer />
+        </>
+    );
+}
